63534b52e4c6f04c6b49a538a851ac79d3b0cff4
EC2 Default User <ec2-user@ip-172-31-38-215.us-east-2.compute.internal>
2021-11-01 17:06:10 +0000

    changes in output messages

--- process.py
+++ process.py
@@ -1,4 +1,4 @@
-import ricartagrawala
+import ricartagrawala as ra
 import argparse
 import time
 
@@ -15,7 +15,7 @@ def parseArgs():
         parser = argparse.ArgumentParser(prog=APP_NAME, usage='%(prog)s [options]')
         parser.add_argument('--firstprocess_addr','-r', type=str, required = False, default = '127.0.0.1', help='firstprocess address')
         parser.add_argument('--firstprocess_port','-i', type=int, default = None, help='firstprocess port')
-        parser.add_argument('--use_time','-u',type=int,required	= True, help="Using resource time")
+        parser.add_argument('--cs_time','-u',type=int,required	= True, help="critical section time [ unit - seconds ]")
         parser.add_argument('--wait_time','-w',type=int,required = True, help="Idle time")
         parser.add_argument('--name','-n',type=str,required =True, help="Unique Node Name")
         parser.add_argument('--addr','-a',type=str,required =False, default = '', help="node address")
@@ -24,29 +24,37 @@ def parseArgs():
  
 class RaTest(object):
 	def __init__(self,args):
-		self.use_time = args.use_time
+		self.use_time = args.cs_time
 		self.wait_time = args.wait_time
 		self.name = args.name
-		self.sponsor = (args.sponsor_addr, args.sponsor_port)
+		self.sponsor = (args.firstprocess_addr, args.firstprocess_port)
 		self.addr = args.addr
 		self.port = args.port
 
 	def runTest(self): 
 		test = ra.RA(self.name,self.addr,self.port)
 		if (self.sponsor[0] != None ) and (self.sponsor[1] !=None):
-			print "NODE::" + self.name + "::INITIALIZATTION"
+			print "PROCESS::" + self.name + "::INITIALIZATTION"
 			test.init(self.sponsor)
 
-		print "NODE::" + self.name + "::READY"
+		print "PROCESS::" + self.name + "::READY"
 		while(True):
-			print "NODE::" + self.name + "::IDLE"
+			print "PROCESS::" + self.name + "::IDLE"
 			time.sleep(self.wait_time)
-			print "NODE::" + self.name + "::ACQUIRING_RESOURCE"
+			print "PROCESS::" + self.name + "::ACQUIRING_RESOURCE [ CRITICAL SECTION] "
 			test.acquire()
-			print "NODE::" + self.name + "::USING_RESOURCE"
+                        print "PROCESS:: DATA STRUCTURE INFO @PROCESS::"+self.name
+                        print "CURRENT SEQUENCE NUMBER::"+str(test.highest_seq_num)
+                        print "CURRENT REPLY COUNT - OUTSTANDING::"+str(test.oustanding_reply_count)
+                        print "DEFFERED::"
+                        print test.reply_deffered
+                        print "AWAITING REPLY::"
+                        print test.awaiting_reply
+
+			print "PROCESS::" + self.name + "::USING_RESOURCE [ IN CRITICAL SECTION ]"
 			time.sleep(self.use_time)
-			print "NODE::" + self.name + "::USING_DONE"		
-			print "NODE::" + self.name + "::RELEASING_RESOURCE"
+			print "PROCESS::" + self.name + "::USING_DONE [ CRITICAL SECTION COMPLETED ]"		
+			print "PROCESS::" + self.name + "::RELEASING_RESOURCE [ RELEASSING FROM CRITICAL SECTION]"
 			test.release()
 
 
